e36f3a8e0977c32e8b8c1fb537a3fc6a2b30f417
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-05-03 21:04:34 -0400

    merge files

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,60 @@ var time = 100;
 var $timeEl = document.querySelector(".timer");
 var timer;
 
+//questions array
+const questions = [
+  {
+    question: 'Commonly used data types do NOT include:',
+    options: [
+      'strings',
+      'booleans',
+      'alerts',
+      'numbers'
+  ],
+    correct: 'alerts'
+  },
+  {
+    question: 'The condition in an if / else statement is enclosed with _____.',
+    options: [
+      'quotes',
+      'curly brackets',
+      'parenthesis',
+      'square brackets'
+    ],
+    correct: 'parenthesis'
+  },
+  {
+    question: 'Arrays in JavaScriptt can be used to store ___.',
+    options: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above'
+    ],
+    correct: 'all of the above'
+  },
+  {
+    question: 'String values must be enclosed within ___ when being assigned to variables',
+    options: [
+      'commas',
+      'curly brackets',
+      'quotes',
+      'parenthesis'
+    ],
+    correct: 'parenthesis'
+  },
+  {
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    options: [
+      'JavaScript',
+      'terminal/bash',
+      'for loops',
+      'console.log()'
+    ]
+    ,
+    correct: 'console.log()'
+  }
+]
 
 
 // function to start the game and the timer
